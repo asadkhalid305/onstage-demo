61ad96eece19198a982812d51256ca7bd4d1bbb0
Asad Ullah Khalid <asadkhalid305@gmail.com>
2026-01-20 12:28:11 +0100

    fix: make base path conditional for better local development experience

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/apps/onstage/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/apps/onstage/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
